0ae7b78b6202f4672c052675a462e4fbbe4c1a81
Grace Taumoefolau <gtaumoe1@jhu.edu>
2018-08-28 15:49:37 -0400

    from afternoon lesson

--- day2-afternoon/00-uniq.py
+++ day2-afternoon/00-uniq.py
@@ -7,8 +7,9 @@ import sys
 
 #gene_names_seen = []
 # create a list for gene names seen
-gene_names_seen = {}
+#gene_names_seen = {}
 # dictionary to help this script run faster
+gene_names_seen = set()
 
 
 for i, line in enumerate(sys.stdin):
@@ -20,7 +21,8 @@ for i, line in enumerate(sys.stdin):
         continue
     else:
         #gene_names_seen.append(gene_name)
-        gene_names_seen[gene_name] = True
+        #gene_names_seen[gene_name] = True
+        gene_names_seen.add(gene_name)
 
 for name in gene_names_seen:
     print(name)
