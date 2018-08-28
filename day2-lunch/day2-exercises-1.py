1704c89b94831aae0d0b1e16d6f7dc794a2ec4e9
Grace Taumoefolau <gtaumoe1@jhu.edu>
2018-08-28 15:03:06 -0400

    2299051

--- day2-lunch/day2-exercises-1.py
+++ day2-lunch/day2-exercises-1.py
@@ -11,21 +11,7 @@ i = 0
 a = 0
 
 for i, line in enumerate(f):
-    if line.startswith("SRR072893"):
+    if line.startswith("SRR"):
         i += 1
-    else:
-        a += 1
         
-print(i, a)
-
-    #else:
-    #    a = a + 1
-    #    print(i, count)
-    #fields = line.rstrip("\r\n").split("\t")
-    #tx_len = int(fields[4]) - int(fields[3])
-    #if tx_len > 10000:
-    #    print(fields[5], tx_len)
-    
-    #print(line.rstrip())
-    #if i > 10:
-    #    break
+print(i)
