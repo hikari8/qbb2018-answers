d033043f4444981ac7ac23b1b07f89b3072b2d84
Grace Taumoefolau <gtaumoe1@jhu.edu>
2018-08-28 15:46:49 -0400

    commit

---
+++ day2-afternoon/00-uniq.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+
+# Print the unique gene names from a t_data.ctab file
+
+
+import sys
+
+#gene_names_seen = []
+# create a list for gene names seen
+gene_names_seen = {}
+# dictionary to help this script run faster
+
+
+for i, line in enumerate(sys.stdin):
+    if i == 0:
+        continue
+    fields = line.rstrip("\r\n").split("\t")
+    gene_name = fields[9]
+    if gene_name in gene_names_seen:
+        continue
+    else:
+        #gene_names_seen.append(gene_name)
+        gene_names_seen[gene_name] = True
+
+for name in gene_names_seen:
+    print(name)
+
+# The code that looked at lists took some time (4.3s)
+# Dictionary took an order of magnitude faster.
