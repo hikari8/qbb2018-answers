5a1d69e9ea18f026c96782d41730b6ce1549c700
Grace Taumoefolau <gtaumoe1@jhu.edu>
2018-08-28 20:30:54 -0400

    ./day2-homework-2.py fly.txt ~/data/results/stringtie/SRR072893/t_data.ctab Nah

--- day2-afternoon/day2-homework-2.py
+++ day2-afternoon/day2-homework-2.py
@@ -1,19 +1,37 @@
 #!/usr/bin/env python3
+# Run with:
 
 import sys
 import numpy as np
 
-#fly_names = {}
+#if len(sys.argv) > 1:
+#    f = open(sys.argv[1])
+#else:
+#    f = sys.stdin
+
+fly_names = {}
 a = 0
     
-for i, line in enumerate(sys.stdin):
+for line in open(sys.argv[1]):
     if "DROME" in line and "FBgn" in line:
         fields = line.rstrip("\r\n").split()
-        #fly_names.update({fields[2]:fields[3]})
-        a += 1
-        print(fields[3] + "\t\t" + fields[2])
+        #a += 1
+        #print(fields[3] + "\t\t" + fields[2])
+        fly_names.update({fields[3]:fields[2]})
+#print(a)
 
-print(a)
-# for key, value in fly_names:
-#     print(key + "\t\t" + value)
+for line in open(sys.argv[2]):
+    if "FBgn" in line:
+        fields = line.rstrip("\r\n").split("\t")
+        fly = fields[8]
+        if fly in fly_names.keys():
+            uniq = fly_names[fly]
+            print(line.rstrip('\r\n') + "\t" + uniq)
+        else:
+            if len(sys.argv) > 3:
+                pass
+            else:
+                print(line.rstrip('\r\n') + "\t" + "Not found")
+                
+
 
